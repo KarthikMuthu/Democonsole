f242b54222471ae752293f7df33f1bfed0e72f92
Saimuthukarthik <connectmuthu28@gmail.com>
2020-03-06 11:59:34 +0530

    Add project files.

---
+++ Democonsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Democonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
